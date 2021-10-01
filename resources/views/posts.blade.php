44cb6be87f6b3c29370fe86cec4bad8b8d850c6f
kl23711579 <kl23711579@gmail.com>
2021-10-01 22:41:30 +1000

    14 Blade: The Absolute Basics

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,18 +4,19 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-    <article>
+    @foreach ($posts as $post)
+{{--        @dd($loop)--}}
+    <article class="{{ $loop->even ?  'mb-6' : '' }}">
         <h1>
-            <a href="/posts/<?= $post->slug ?>">
-                <?= $post->title; ?>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
             </a>
         </h1>
         <div>
-            <?= $post->excerpt ?>
+            {{ $post->excerpt }}
         </div>
     </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
 
 
