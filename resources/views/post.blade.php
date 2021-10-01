e868f04e65fa844b4490f7d4f353e702b8a9dfaf
kl23711579 <kl23711579@gmail.com>
2021-10-01 19:06:25 +1000

    08

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,13 +5,7 @@
 
 <body>
     <article>
-        <h1>
-            <a href="/post">My First Post</a>
-        </h1>
-
-        <p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis culpa dignissimos dolore expedita fuga illo laboriosam magnam magni nobis, quo repellat rerum tempora vitae! Consequatur dolorum eaque maiores non tempora!</span><span>Ad alias aliquam aliquid beatae culpa dolorum ducimus enim eos et, eveniet exercitationem fugit hic id magnam nam nesciunt odio pariatur quod repellendus temporibus tenetur vel velit? Doloremque, quaerat, recusandae.</span><span>Atque exercitationem natus, necessitatibus officiis possimus quo sapiente? Corporis dignissimos, dolorem doloribus dolorum explicabo laboriosam nihil provident totam? Aperiam cum delectus dolor earum enim error laborum neque, quasi quia quo!</span><span>Accusamus ad debitis dolorem doloribus earum enim id itaque laborum laudantium, magnam nihil, odit perspiciatis rerum vero voluptate? Aliquam aperiam earum illum iure? Deleniti, eius non quasi quidem vero voluptatibus.</span><span>Accusantium, alias cumque dignissimos dolorum ea earum, eligendi excepturi fugit illum iste maiores nam nemo quibusdam quisquam sit ullam vel. Ad dolore ipsa iusto magni non omnis tempora vel velit.</span>
-        </p>
+        <?= $post; ?>
     </article>
     <a href="/">Go Back</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 <body>
     <article>
         <h1>
-            <a href="/post">My First Post</a>
+            <a href="/posts/my-first-post">My First Post</a>
         </h1>
 
         <p>
@@ -17,7 +17,7 @@
 
     <article>
         <h1>
-            <a href="/post">My Second Post</a>
+            <a href="/posts/my-second-post">My Second Post</a>
         </h1>
 
         <p>
@@ -27,7 +27,7 @@
 
     <article>
         <h1>
-            <a href="/post">My Third Post</a>
+            <a href="/posts/my-third-post">My Third Post</a>
         </h1>
 
         <p>
