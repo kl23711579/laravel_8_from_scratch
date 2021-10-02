cf676a527bd343b6be4b84921b684d99dc826260
kl23711579 <kl23711579@gmail.com>
2021-10-02 18:19:53 +1000

    28 Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,40 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-
-
-         $user = User::factory()->create();
-         $personal = Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal'
-         ]);
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        $user = User::factory()->create([
+            'name' => "JohnJoe"
         ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'category_id' => $family->id,
-            'user_id' => $user->id,
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan lacus vel facilisis volutpat. Mollis aliquam ut porttitor leo a diam sollicitudin. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. In tellus integer feugiat scelerisque. Amet justo donec enim diam. Ultrices in iaculis nunc sed. Laoreet suspendisse interdum consectetur libero. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque.',
-        ]);
-
-        Post::create([
-            'title' => 'My Work Post',
-            'slug' => 'my-second-post',
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan lacus vel facilisis volutpat. Mollis aliquam ut porttitor leo a diam sollicitudin. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. In tellus integer feugiat scelerisque. Amet justo donec enim diam. Ultrices in iaculis nunc sed. Laoreet suspendisse interdum consectetur libero. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque.',
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
