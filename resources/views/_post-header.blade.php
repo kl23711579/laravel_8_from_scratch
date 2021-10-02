22668e4104687933551498d6346532b4688632ab
kl23711579 <kl23711579@gmail.com>
2021-10-03 04:55:43 +1000

    38 Search (The Cleaner Way)

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -52,11 +52,13 @@
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text"
-                       name="search"
-                       placeholder="Find something"
-                       class="bg-transparent placeholder-black font-semibold text-sm"
-                       value="{{ request('search') }}">
+                <label>
+                    <input type="text"
+                           name="search"
+                           placeholder="Find something"
+                           class="bg-transparent placeholder-black font-semibold text-sm"
+                           value="{{ request('search') }}" />
+                </label>
             </form>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -17,26 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-    if (request('search')) {
-        $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function(Post $post) { //Post::where('slug', $post)->firstOrFail()
-    // find a post by its slug and pass it to a view called 'post'
-    return view('post', [
-        'post' => $post
-    ]);
-})->name('category');
+Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('category');
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
