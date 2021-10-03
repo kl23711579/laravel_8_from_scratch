e4325c5192597da117ff5edeb9954c2b078b86fa
kl23711579 <kl23711579@gmail.com>
2021-10-03 13:44:23 +1000

    44 Laughably simple Pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,10 +7,13 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request('category') === null">All</x-dropdown-item>
+    <x-dropdown-item
+        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+        :active="request('category') === null">All
+    </x-dropdown-item>
     @foreach($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             {{--                        is current request match url--}}
             :active="request('category') === $category->slug"
         >{{ ucwords($category->name) }}</x-dropdown-item>
