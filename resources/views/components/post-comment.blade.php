1d5992dfa1c3a4f0475f3ca06cc3b4ffb819687b
kl23711579 <kl23711579@gmail.com>
2021-10-26 22:00:47 +1000

    52 Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,14 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted <time>8 months ago</time> </p>
+        </header>
+
+        <p>Vel quo officiis est et sed. Et officia in ipsam sed laboriosam nihil fugit. Ut est non dignissimos est. Placeat autem ex aperiam qui repudiandae fuga.</p>
+    </div>
+</article>
