1ed391a04583396bd3cafbfb30854fb360ce2d5f
kl23711579 <kl23711579@gmail.com>
2021-10-27 21:59:20 +1000

    54 Make the Comments Section Dynamic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -45,6 +45,11 @@ class Post extends Model
 //        );
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function category()
     {
         // hasOne, hasMany, belongsTo, belongsToMany
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,14 +1,15 @@
+@props(['comment'])
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
-            <p class="text-xs">Posted <time>8 months ago</time> </p>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+            <p class="text-xs">Posted <time>{{ $comment->created_at }}</time> </p>
         </header>
 
-        <p>Vel quo officiis est et sed. Et officia in ipsam sed laboriosam nihil fugit. Ut est non dignissimos est. Placeat autem ex aperiam qui repudiandae fuga.</p>
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
