e2eb66496a0cb399df8b0dec87ad5b08bf5e07a9
kl23711579 <kl23711579@gmail.com>
2021-11-03 19:04:48 +1000

    60 Extract a Newsletter Service

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,16 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('newsletter', function () {
-    request()->validate(['email' => 'required|email']);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us5'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember("f4ed228e1b", [
-            'email_address' => request('email'),
-            'status' => "subscribed"
-        ]);
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => "This email could not be added into our newsletter list."
-        ]);
-    }
-
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter.');
-});
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('category');
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store']);
 
